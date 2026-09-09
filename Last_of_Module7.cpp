0cc40005b0b2c5b86a89e9ed5189c54729e05d87
Mohammed <Mohammed@MOHAMMED-PC>
2026-09-09 07:19:06 +0300

    Update .gitignore and modify main program output

--- Last_of_Module7.cpp
+++ Last_of_Module7.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-	cout << "Hello, World!" << endl;
+    cout << "Student: Ahmad" << endl;
+    cout << "Course: Git and GitHub" << endl;
+    cout << "Favorite Language: C++" << endl;
+    cout << "Mini Challenge Completed!" << endl;
 
-	return 
+    return 0;
 }
